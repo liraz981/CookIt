a6b0a17c9c842d611be0a3b89ed264cc04339b3a
Liraz Mizrachi <liraz981@cs.colman.ac.il>
2021-01-09 20:22:18 +0200

    create navigation

--- app/src/main/java/com/liraz/cookit/activities/MainActivity.java
+++ app/src/main/java/com/liraz/cookit/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.liraz.cookit;
+package com.liraz.cookit.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.liraz.cookit.R;
 
 public class MainActivity extends AppCompatActivity {
 
