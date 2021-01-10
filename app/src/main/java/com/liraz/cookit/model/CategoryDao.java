e858be0956843ac294a2c61e831552df35b968e9
meitalzaguri95 <MeitalMoyal>
2021-01-10 16:35:07 +0200

    CategoryModel

--- app/src/main/java/com/liraz/cookit/model/CategoryDao.java
+++ app/src/main/java/com/liraz/cookit/model/CategoryDao.java
@@ -1,9 +1,32 @@
 package com.liraz.cookit.model;
 
+import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import java.util.List;
 
 @Dao
 public interface CategoryDao
 {
+    @Query("select * from Category")
+    LiveData<List<Category>> getAllRecipes();
+
+    //inserting and updating
+    //... is used when we don't know how many arguments will pass..it can be 0 category, 1 or more...
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertAllCategories(Category...categories);
+
+    @Delete
+    void deleteCategories(Category category);
+
+    @Query("select exists(select * from Category where categoryId = :categoryId)")
+    boolean isCategoryExists(String categoryId);
+
+    @Query("delete from Category where categoryId = :categoryId")
+    void deleteByCategoryId(String categoryId);
 
 }
