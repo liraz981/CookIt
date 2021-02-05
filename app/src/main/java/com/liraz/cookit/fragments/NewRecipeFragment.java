c3ee8eddb991596fe0436d1287686350fcf6283d
Lee Lisker <Lee Lisker@DESKTOP-SQAJ41L>
2021-02-05 16:03:51 +0200

    register and login

--- app/src/main/java/com/liraz/cookit/fragments/NewRecipeFragment.java
+++ app/src/main/java/com/liraz/cookit/fragments/NewRecipeFragment.java
@@ -6,12 +6,12 @@ import androidx.fragment.app.Fragment;
 
 public class NewRecipeFragment extends Fragment {
 
-    Spinner spinner = (Spinner) findViewById(R.id.spinner);
+//    Spinner spinner = (Spinner) findViewById(R.id.spinner);
     // Create an ArrayAdapter using the string array and a default spinner layout
-    ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-            R.array.planets_array, android.R.layout.simple_spinner_item);
+ //   ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+//            R.array.planets_array, android.R.layout.simple_spinner_item);
 // Specify the layout to use when the list of choices appears
-adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 // Apply the adapter to the spinner
-spinner.setAdapter(adapter);
+//spinner.setAdapter(adapter);
 }
