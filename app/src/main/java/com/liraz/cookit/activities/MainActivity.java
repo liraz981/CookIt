ee8476a889ec587e92c83d733065cff386b2e4a4
Liraz Mizrachi <liraz981@cs.colman.ac.il>
2021-02-06 18:18:14 +0200

    fix nav graph

--- app/src/main/java/com/liraz/cookit/activities/MainActivity.java
+++ app/src/main/java/com/liraz/cookit/activities/MainActivity.java
@@ -8,6 +8,8 @@ import com.liraz.cookit.R;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
