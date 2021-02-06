75a88d991601dbe241fb1bd0432a805d81b4b98c
Liraz Mizrachi <liraz981@cs.colman.ac.il>
2021-02-06 20:20:41 +0200

    nav graph

--- app/src/main/java/com/liraz/cookit/fragments/MainFragment.java
+++ app/src/main/java/com/liraz/cookit/fragments/MainFragment.java
@@ -3,16 +3,19 @@ package com.liraz.cookit.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.liraz.cookit.R;
 
 public class MainFragment extends Fragment {
 
 
+    Button btn;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -21,6 +24,13 @@ public class MainFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_main, container, false);
 
+        btn = view.findViewById(R.id.button2);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(view).navigate(R.id.action_mainFragment_to_newRecipeFragment3);
+            }
+        });
 
 
         return view;
